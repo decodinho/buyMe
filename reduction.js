f13ac64f9920e194fcc61052b23e804878774b81
decodinho <116439424+decodinho@users.noreply.github.com>
2026-01-06 00:12:18 +0100

    Update reduction.js

--- reduction.js
+++ reduction.js
@@ -1,1 +1,10 @@
-
+window.reduction = async function(prix,redux) {
+  
+  try {
+    // CORRECTION : Ajout de https:// obligatoire
+    let prixFinal = prix(1-(redux)/100)
+    return prixFinal
+  } catch (err) {
+    console.error("pas de prix :", err);
+  }
+};
